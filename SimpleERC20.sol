906a119191b5f8d5994c4fedf4f5dbcd1fa94204
adityatiwari-art <130775596+adityatiwari-art@users.noreply.github.com>
2024-06-30 19:00:55 +0530

    Update SimpleERC20.sol

--- SimpleERC20.sol
+++ SimpleERC20.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SimpleToken is ERC20{
+    constructor() ERC20("MYTOKEN","MTK"){
+    _mint(msg.sender,100*10**decimals());
+    }
+}

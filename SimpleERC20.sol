aab18e8bea732adfd1aa3d3704f8586628cb5612
adityatiwari-art <130775596+adityatiwari-art@users.noreply.github.com>
2024-07-04 20:40:42 +0530

    Update SimpleERC20.sol

--- SimpleERC20.sol
+++ SimpleERC20.sol
@@ -3,8 +3,17 @@ pragma solidity ^0.8.26;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SimpleToken is ERC20{
-    constructor() ERC20("MYTOKEN","MTK"){
-    _mint(msg.sender,100*10**decimals());
+contract SimpleToken is ERC20 {
+    constructor() ERC20("MYTOKEN", "MTK") {
+        _mint(msg.sender, 100 * 10**decimals());
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 }
